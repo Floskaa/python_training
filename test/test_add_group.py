a26c67edf5bde4a1997788af770115b686c17042
Floskaa <floskaa@gmail.com>
2020-08-25 17:29:23 +0300

    Create new class SessionHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,12 +11,12 @@ def app(request):
     return fixture
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_gruop(Group(name="qwertrvf", header="cfserfersc", footer="wefawerfaee"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_gruop(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
