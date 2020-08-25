4ac0ad4747dcb765143d39e543ada8dba37e8ef1
Floskaa <floskaa@gmail.com>
2020-08-25 19:08:45 +0300

    rename metod. Test PASSED

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,13 +12,13 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.create_contact(Contact(firstname="Sofia", middlename="Ivanovna", lastname="Petrova", nickname="Sofi",
-                                    title="prosto test", company="Romashka", address="Kirova str 5",
-                                    home="1333434343",
-                                    mobile="8923445445", work="2343434444", fax="8923232323",
-                                    email="sofi@gmail.com",
-                                    email2="sofi2@gmail.ru", email3="sofi3@mail.ru", homepage="sofi.net",
-                                    address2="Pervaya str 4",
-                                    phone2="1243", note="test contact"))
+    app.contact.create(Contact(firstname="Sofia", middlename="Ivanovna", lastname="Petrova", nickname="Sofi",
+                               title="prosto test", company="Romashka", address="Kirova str 5",
+                               home="1333434343",
+                               mobile="8923445445", work="2343434444", fax="8923232323",
+                               email="sofi@gmail.com",
+                               email2="sofi2@gmail.ru", email3="sofi3@mail.ru", homepage="sofi.net",
+                               address2="Pervaya str 4",
+                               phone2="1243", note="test contact"))
     app.session.logout()
 
