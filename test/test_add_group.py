2ffffbef478e587b218bfaf6db5c38e8849ef37d
Floskaa <floskaa@gmail.com>
2020-08-25 18:06:10 +0300

    Create new class GroupHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,11 +12,10 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_gruop(Group(name="qwertrvf", header="cfserfersc", footer="wefawerfaee"))
+    app.group.create(Group(name="qwertrvf", header="cfserfersc", footer="wefawerfaee"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_gruop(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
-
