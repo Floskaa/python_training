3ba2e2fd775bce308a42401e506924d7331fff1b
Floskaa <floskaa@gmail.com>
2020-08-28 18:30:59 +0300

    Add test_edit_first_group and test_edit_first_contact

--- fixture/group.py
+++ fixture/group.py
@@ -20,7 +20,7 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
-    def test_edit_first_group(self, group):
+    def edit_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
         # select first group
@@ -38,7 +38,7 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
-        wd.find_element_by_name("submit").click()
+        wd.find_element_by_name("update").click()
 
     def delete_first_group(self):
         wd = self.app.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -7,6 +7,7 @@ def test_add_group(app):
     app.group.create(Group(name="qwertrvf", header="cfserfersc", footer="wefawerfaee"))
     app.session.logout()
 
+
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
