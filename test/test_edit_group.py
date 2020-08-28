aec6ba2945de98eeb5ae78ac0e1ed533457b740c
Floskaa <floskaa@gmail.com>
2020-08-28 14:03:04 +0300

    Add test_delete_first_contact

---
+++ test/test_edit_group.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+def test_edit_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="New group", header="12345", footer="testtesttest"))
+    app.session.logout()
