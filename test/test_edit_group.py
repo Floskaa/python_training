81252b2752a9f878ba15895a40fea00d37380053
Floskaa <floskaa@gmail.com>
2020-09-07 18:50:21 +0300

    preconditions for group test

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,4 +3,6 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
-    app.group.edit_first_group(Group(name="New group", header="12345", footer="testtesttest"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
+    app.group.edit_first_group(Group(name="New group 322", header="12345", footer="test322test"))
