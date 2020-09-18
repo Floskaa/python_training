4131ac438ce1e1cf1bac188f197004801b4fded5
Floskaa <floskaa@gmail.com>
2020-09-18 19:35:29 +0300

    add chek for edit group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,9 +3,13 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
-    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    app.group.edit_first_group(Group(name="New group 322", header="12345", footer="test322test"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="New group 322", header="12345", footer="test322test")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
