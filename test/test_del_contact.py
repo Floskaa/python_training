49489dc644f6113ac44377b4b7e9ae8a2f25ad73
Floskaa <floskaa@gmail.com>
2020-09-18 23:26:27 +0300

    add chek for contact tests

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -5,4 +5,7 @@ from model.contact import Contact
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create_for_edit_contact(Contact(firstname="Test"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,6 +5,8 @@ from model.contact import Contact
 def test_modify_contact_name(app):
     if app.contact.count() == 0:
         app.contact.create_for_edit_contact(Contact(firstname="Test"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(firstname="New update contact"))
-
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
 
