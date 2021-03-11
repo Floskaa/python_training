5fc9244910c6655cf04dedb9d0db4bfc1fd9fecf
Floskaa <floskaa@gmail.com>
2021-03-11 14:23:25 +0300

    13. Add Random in tests

--- fixture/group.py
+++ fixture/group.py
@@ -51,21 +51,29 @@ class GroupHelper:
 
     def delete_first_group(self):
         wd = self.app.wd
-        self.open_groups_page()
-        self.select_first_group()
-        # submit deletion
-        wd.find_element_by_name("delete").click()
-        self.return_to_groups_page()
-        self.group_cache = None
+        self.delete_group_by_index(0)
 
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
-    def modify_first_group(self, new_group_data):
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
+    def modify_group_by_index(self, index, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_index(index)
         # open modification form
         wd.find_element_by_name("edit").click()
         # fill group form
@@ -74,6 +82,9 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
         self.group_cache = None
+
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
 
     def return_to_groups_page(self):
         wd = self.app.wd
