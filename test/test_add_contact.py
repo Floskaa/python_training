0beb53f2c8755e5840467ea464bbb1a11230bf28
Floskaa <floskaa@gmail.com>
2021-03-14 13:06:07 +0300

    15. Add parametrize data to create groups and contacts tests

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -23,14 +23,6 @@ testdata = [Contact(firstname="", lastname="", address="")] + [
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
 def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    # contact = Contact(firstname="Sofia", middlename="Ivanovna", lastname="Petrova", nickname="Sofi",
-    #                            photo="C:\\Devel\\Andru.jpg", title="prosto test", company="Romashka",
-    #                            address="Kirova str 5", home="1333434343",
-    #                            mobile="8923445445", work="2343434444", fax="8923232323", email="sofi@gmail.com",
-    #                            email2="sofi2@gmail.ru", email3="sofi3@mail.ru", homepage="sofi.net",
-    #                            bday="19", bmonth="April", byear="1989", aday="6", amonth="May", ayear="1999",
-    #                            address2="Pervaya str 4",
-    #                            phone2="1243", note="test contact")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
