19f2a5aabd08b14aae2ad06eb8972a8ec05ae132
Floskaa <floskaa@gmail.com>
2021-03-17 10:28:03 +0300

    18. add generator for test groups in json format

--- generator/group.py
+++ generator/group.py
@@ -3,7 +3,7 @@ from model.group import Group
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -24,7 +24,7 @@ for o, a in opts:
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
@@ -36,8 +36,8 @@ testdata = [Group(name="", header="", footer="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
-    # функция dumps превращает некоторую структуру данных в строку в форате json
-    # функция default помогает преобразовывать данные, когда json не знает как именно преобразовывать.
-    # Она преобразовывает наш файл в словарь. indent - форматирует текст для читабельности
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
+
+
 
